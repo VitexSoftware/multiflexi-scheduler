c7827d5b70deaed5edb3ff07c3e245e24e82c7bc
CyberVitexus <info@vitexsoftware.cz>
2025-07-31 12:14:00 +0200

    Initial import of multiflexi-scheduler package for Debian. This includes the necessary Debian packaging files such as control, changelog, and service definitions. The main functionality is implemented in the daemon and scheduler scripts, which manage the scheduling of MultiFlexi jobs. Additional files for configuration, installation, and maintenance scripts are also included.

---
+++ src/daemon.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the MultiFlexi package
+ *
+ * https://multiflexi.eu/
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MultiFlexi;
+
+require_once '../vendor/autoload.php';
+\Ease\Shared::init(['DB_CONNECTION', 'DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'], '../.env');
+$daemonize = (bool) \Ease\Shared::cfg('MULTIFLEXI_DAEMONIZE', true);
+$loggers = ['syslog', '\MultiFlexi\LogToSQL'];
+
+if (\Ease\Shared::cfg('ZABBIX_SERVER') && \Ease\Shared::cfg('ZABBIX_HOST') && class_exists('\MultiFlexi\LogToZabbix')) {
+    $loggers[] = '\MultiFlexi\LogToZabbix';
+}
+
+if (strtolower(\Ease\Shared::cfg('APP_DEBUG', 'false')) === 'true') {
+    $loggers[] = 'console';
+}
+
+\define('EASE_LOGGER', implode('|', $loggers));
+\Ease\Shared::user(new \Ease\Anonym());
+
+function waitForDatabase(): void
+{
+    $try = 0;
+
+    while (true) {
+        try {
+            $testScheduler = new Scheduler();
+            $testScheduler->getCurrentJobs(); // Try a simple query
+            unset($testScheduler);
+
+            break;
+        } catch (\Throwable $e) {
+            if ($try++ < 10) {
+                error_log('Database unavailable: '.$e->getMessage());
+                sleep(30);
+            } else {
+                throw new \RuntimeException('Database unavailable: '.$e->getMessage());
+            }
+        }
+    }
+}
+
+waitForDatabase();
+$scheduler = new Scheduler();
+$scheduler->logBanner('MultiFlexi Schedule Daemon started');
+
+date_default_timezone_set('Europe/Prague');
+
+function scheduleIntervalJobs(string $interval): void
+{
+    $companer = new Company();
+    $companies = $companer->listingQuery();
+
+    $jobber = new Job();
+    $runtemplate = new \MultiFlexi\RunTemplate();
+
+    foreach ($companies as $company) {
+        LogToSQL::singleton()->setCompany($company['id']);
+
+        $appsForCompany = $runtemplate->getColumnsFromSQL(['id', 'interv', 'delay', 'name', 'executor'], ['company_id' => $company['id'], 'interv' => $interval, 'active' => true]);
+
+        if (empty($appsForCompany) && ($interval !== 'i')) {
+            $companer->addStatusMessage(sprintf(_('No applications to run for %s in interval %s'), $company['name'], $interval), 'debug');
+        } else {
+            if (strtolower(\Ease\Shared::cfg('APP_DEBUG','false')) === 'true') {
+                $jobber->addStatusMessage(sprintf(_('%s Scheduler interval %s begin'), $company['name'], $interval), 'debug');
+            }
+
+            foreach ($appsForCompany as $runtemplateData) {
+                if (null !== $interval && ($interval !== $runtemplateData['interv'])) {
+                    continue;
+                }
+
+                $startTime = new \DateTime();
+                $jobber->addStatusMessage('Now Is '.$startTime->format('Y-m-d H:i:s'), 'debug');
+
+                if (empty($runtemplateData['delay']) === false) {
+                    $startTime->modify('+'.$runtemplateData['delay'].' seconds');
+                    $jobber->addStatusMessage('Adding Startup delay  +'.$runtemplateData['delay'].' seconds to '.$startTime->format('Y-m-d H:i:s'), 'debug');
+                }
+
+                $jobber->prepareJob($runtemplateData['id'], new ConfigFields(''), $startTime, $runtemplateData['executor'], RunTemplate::codeToInterval($interval));
+                $jobber->scheduleJobRun($startTime);
+                $jobber->addStatusMessage('🧩 #'.$jobber->application->getMyKey()."\t".$jobber->application->getRecordName().':'.$runtemplateData['name'].' (runtemplate #'.$runtemplateData['id'].') - '.sprintf(_('Launch %s for 🏣 %s'), $startTime->format(\DATE_RSS), $company['name']));
+            }
+
+            if (strtolower(\Ease\Shared::cfg('APP_DEBUG','false')) === 'true') {
+                $jobber->addStatusMessage(sprintf(_('%s Scheduler interval %s end'), $company['name'], RunTemplate::codeToInterval($interval)), 'debug');
+            }
+        }
+    }
+}
+
+// Výchozí časy pro další spuštění jednotlivých úloh
+$nextMinute = (new \DateTime())->modify('+1 minute')->setTime((int) date('H'), (int) date('i') + 1, 0);
+$nextHour = (new \DateTime())->modify('+1 hour')->setTime((int) date('H') + 1, 0, 0);
+$nextDay = (new \DateTime('tomorrow'))->setTime(0, 0, 0);
+$nextWeek = clone $nextDay;
+
+while ((int) $nextWeek->format('w') !== 0) {
+    $nextWeek->modify('+1 day');
+}
+
+$nextMonth = (new \DateTime('first day of next month'))->setTime(0, 0, 0);
+$nextYear = (new \DateTime('first day of January next year'))->setTime(0, 0, 0);
+
+do {
+    $intervalOccured = '';
+    $now = new \DateTime();
+
+    if ($now >= $nextMinute) { // Minutely
+        $nextMinute->modify('+1 minute');
+        scheduleIntervalJobs('i');
+    }
+
+    if ($now >= $nextHour) { // Hourly
+        $nextHour->modify('+1 hour');
+        scheduleIntervalJobs('h');
+    }
+
+    if ($now >= $nextDay) { // Daily
+        $nextDay->modify('+1 day');
+        scheduleIntervalJobs('d');
+
+        if ((int) $now->format('w') === 0) { // Weekly
+            $nextWeek->modify('+7 days');
+            scheduleIntervalJobs('w');
+        }
+
+        $tomorrow = (clone $now)->modify('+1 day');
+
+        if ((int) $tomorrow->format('j') === 1) { // Monthly
+            $nextMonth = (new DateTime('first day of next month'))->setTime(0, 0, 0);
+            scheduleIntervalJobs('m');
+        }
+
+        if ((int) $tomorrow->format('z') === 0) { // Yearly
+            $nextYear = (new DateTime('first day of January next year'))->setTime(0, 0, 0);
+            scheduleIntervalJobs('y');
+        }
+    }
+
+    usleep(100_000); // 0.1 sekundy, šetří CPU
+} while ($daemonize);
+
+$scheduler->logBanner('MultiFlexi Daemon ended');
